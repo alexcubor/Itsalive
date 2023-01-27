37d0ea3733c8fe9437ab74409959b10742ccb4a3
alexcubor <sdiz@ya.ru>
2023-01-27 14:48:02 +0300

    Init meta project

--- nuke/scripts/init.py
+++ nuke/scripts/init.py
@@ -1,2 +1,13 @@
 import nuke
 import sys
+import os
+
+
+nuke.pluginAddPath(os.path.dirname(__file__) + "/meta_project")
+from imp import reload
+import bahMT
+reload(bahMT)
+from bahMT import bah_mt
+mw = bah_mt()
+#mw.show()
+nukescripts.registerWidgetAsPanel('bah_mt', 'bah_mt','com.aaa.bah_mt')
