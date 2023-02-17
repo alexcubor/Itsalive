d862507cd46f0b9a1d3051af5fe35b16704f4d4c
alexcubor <sdiz@ya.ru>
2023-02-17 22:54:54 +0300

    Добавлен автозапуск плагинов

--- maya/mayarender.cmd
+++ maya/mayarender.cmd
@@ -2,6 +2,7 @@
 
 call %CGRU_LOCATION%\software_setup\setup_maya.cmd
 set "MAYA_MODULE_PATH=\\alpha\tools\Itsavile\tools\Itsalive\maya\plugins\MSLiveLink;%MAYA_MODULE_PATH%"
+set "PYTHONPATH=\\alpha\tools\Itsavile\tools\Itsalive\maya\scripts;%PYTHONPATH%"
 
 if "%GPU_LIST%" == "" (
     "%APP_DIR%\bin\Render.exe" %*
